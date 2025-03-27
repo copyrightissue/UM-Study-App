c80f490e7f8e767d06e3c9e5e711865d7e3e44c5
Gavin <gavinwaynebrown@gmail.com>
2025-03-27 17:04:12 -0600

    slow e2e

--- playwright.config.js
+++ playwright.config.js
@@ -3,5 +3,10 @@ const { defineConfig } = require('@playwright/test');
 module.exports = defineConfig({
   testDir: 'functions/__e2e__',  // Set the directory for Playwright tests
   testIgnore: ['**/*.test.js'], // Ignore Jest test files
+  Use:{
+    launchOptions: {
+      slowMo: 2_000, // Add a 500ms delay between actions
+    },
+  },
 });
 
