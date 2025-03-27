ce6881c15b92e56bf103c810895e2d9e9c552a4d
Jake-M-Short <jakemshort@yahoo.com>
2025-03-26 22:17:19 -0600

    tiny fix
    made a minor fix for class creation export.

--- um-study-app/src/pages/classcreation.tsx
+++ um-study-app/src/pages/classcreation.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import StudyBuddyLogin from "@/components/classCreation";
-import StudyBuddyClassCreation from "@/components/classCreation";
+import ClassCreation from "@/components/classCreation"; 
 
-const StudyBuddyClassCreationPage: React.FC = () => {
-  return <StudyBuddyClassCreationPage/>;
+const ClassCreationPage: React.FC = () => {
+  return <ClassCreation/>;
 };
 
-export default <StudyBuddyClassCreation/>;
+export default ClassCreationPage;
