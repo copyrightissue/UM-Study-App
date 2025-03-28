fa893e44e9f90ef9546311054bb824391934e233
Gavin <gavinwaynebrown@gmail.com>
2025-03-27 22:59:06 -0600

    Testing e2e site navigation

--- playwright.config.js
+++ playwright.config.js
@@ -3,9 +3,14 @@ const { defineConfig } = require('@playwright/test');
 module.exports = defineConfig({
   testDir: 'functions/__e2e__',  // Set the directory for Playwright tests
   testIgnore: ['**/*.test.js'], // Ignore Jest test files
-  Use:{
+  projects: [
+    { name: 'Signup', testMatch: /signup\.spec\.js/ },
+    { name: 'Login', testMatch: /login\.spec\.js/, dependencies: ['Signup'] },
+    { name: 'CreateClass', testMatch: /createClass\.spec\.js/, dependencies: ['Signup'] },
+  ],
+  use:{
     launchOptions: {
-      slowMo: 2_000, // Add a 500ms delay between actions
+      slowMo: process.env.SLOMO ? 1_000 : 0, // Add a 500ms delay between actions
     },
   },
 });
